ac109ee8acd24a9da0f01f50c6d501bdf10a3160
BCJuan <jbcarazo@gmail.com>
2018-07-15 17:38:04 +0200

    added save process and date verification

--- data.R
+++ data.R
@@ -12,23 +12,31 @@ if (file.exists("./csv/data/adj_IBEX35.csv")){
   adj_frame <-  read_csv("./csv/data/adj_IBEX35.csv", guess_max = 2000, 
                        col_types = cols(.default= col_double(), Dates=col_date()))
   
-  use_date <-  adj_frame$Dates[length(adj_frame$Dates)] + days(1)
-  
-  getSymbols(members$`Ticker symbol`, from=use_date)
-  
-  frame_to_add <- do.call(merge, lapply(members$`Ticker symbol`, function(x) Ad(get(x))))
-  
-  dates <- index(frame_to_add)
-  
-  frame_to_add <- as_tibble(frame_to_add)
-  
-  frame_to_add <- frame_to_add %>%
-    mutate(Dates = dates) %>%
-    select(Dates, ABE.MC.Adjusted:VIS.MC.Adjusted)
-  
-  Adj_tibble <- bind_rows(adj_frame,frame_to_add)
-  
-  write_csv(Adj_tibble, path = "./csv/data/adj_IBEX35.csv")
+  tod <- wday(as_date(now()),week_start = getOption("lubridate.week.start", 1))
+  last_day <- wday(adj_frame$Dates[length(adj_frame$Dates)],week_start = getOption("lubridate.week.start", 1))
+  if (last_day==5 && tod >5){print("Already up to date")
+    } else {
+    if (last_day==5 && tod ==2){
+      use_date <-  adj_frame$Dates[length(adj_frame$Dates)] + days(3)
+      } else {
+        use_date <-  adj_frame$Dates[length(adj_frame$Dates)] + days(1)
+        }
+      
+    getSymbols(members$`Ticker symbol`, from=use_date)
+    
+    frame_to_add <- do.call(merge, lapply(members$`Ticker symbol`, function(x) Ad(get(x))))
+    
+    dates <- index(frame_to_add)
+    
+    frame_to_add <- as_tibble(frame_to_add)
+    
+    frame_to_add <- frame_to_add %>%
+      mutate(Dates = dates) %>%
+      select(Dates, ABE.MC.Adjusted:VIS.MC.Adjusted)
+    
+    Adj_tibble <- bind_rows(adj_frame,frame_to_add)
+    
+    write_csv(Adj_tibble, path = "./csv/data/adj_IBEX35.csv")}
   
 } else {
   getSymbols(members$`Ticker symbol`, from="2016-01-01")
--- wallet.py
+++ wallet.py
@@ -6,15 +6,16 @@ Created on Wed Jul 11 12:23:07 2018
 @author: blue
 """
 
-import argparse
+from argparse import ArgumentParser
 import numpy as np
-import scipy.optimize as sco
-import pandas as pd
+from scipy.optimize import minimize 
+from pandas import to_datetime, Timedelta, read_csv, concat
+from os.path import exists
 
 OBJECTIVE="maxsharpe"
 
 def arg_parser():
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     parser.add_argument("--quant", type=float, default=1000,
                         help="Quantity to invest")
     parser.add_argument("--obj", type=str, default=OBJECTIVE,
@@ -26,8 +27,7 @@ def arg_parser():
     parser.add_argument("--fee-abs", type=float, 
                         help="Absolute fees (1% is 0.01)")
     parser.add_argument("--fee-rel", type=float,
-                        help="Relatie fee (1% is 0.01)")
-    
+                        help="Relative fee (1% is 0.01)")
     
     return parser.parse_args()
 
@@ -52,7 +52,7 @@ def optimizer(prices, inv_quant,rets, func):
     init = np.random.random(len(prices))
     init /= np.sum(init)
     
-    opts =  sco.minimize(func, init, method = "SLSQP", constraints=cons, bounds=bnds,
+    opts =  minimize(func, init, method = "SLSQP", constraints=cons, bounds=bnds,
                         options={'maxiter':5000}, args=(rets))
     
     if not opts['success']:
@@ -67,9 +67,17 @@ def optimizer(prices, inv_quant,rets, func):
 
 def data_obt(rang):
     
-    frame = pd.read_csv("./csv/data/adj_IBEX35.csv")
-    yesterday = (pd.to_datetime('Today') - pd.Timedelta('1 days')).strftime('%Y-%m-%d')
-    if  not frame.iloc[-1]["Dates"] == yesterday:
+    if not exists("./csv/data/adj_IBEX35.csv"):
+        print("Please download the data before executing the program")
+        raise AssertionError()
+    else:
+        frame = read_csv("./csv/data/adj_IBEX35.csv")
+    
+    yesterday = (to_datetime('Today') - Timedelta('1 days')).strftime('%Y-%m-%d')
+    yesterday_use = to_datetime('Today') - Timedelta('1 days')
+    if  not frame.iloc[-1]["Dates"] == yesterday and yesterday_use.weekday() not in list([5,6]):
+        print("ATTENTION")
+        print("-"*30)
         print("-"*30)
         print("Data not updated. Please run R file to obtain recent prices")
         print("LAst day in data: {}".format(frame.iloc[-1]["Dates"]))
@@ -93,6 +101,54 @@ def fee_calculator( money, quantities, absolute=0.0, relative=0.0):
     total = result_absolute +   result_relative
     return result_absolute,result_relative, total
 
+def saving(quantos, pricos):
+    
+    if not exists("./accountability.csv"):
+        tabular = quantos.append(pricos).to_frame().T
+        o_cols = tabular.columns
+        n_cols = []
+        for i in range(len(o_cols)):
+            name = o_cols[i].split(".")
+            if i< len(o_cols)/2:
+                n_name = str(name[0] + "Quant")
+            else:
+                n_name = str(name[0] +"Price")
+            n_cols.append(n_name)
+        
+        tabular.columns = n_cols
+        tabular.to_csv("./accountability.csv")
+    else:
+        tab  = read_csv("./accountability.csv", index_col = 0)
+        tabular = quantos.append(pricos).to_frame().T
+        o_cols = tabular.columns
+        n_cols = []
+        for i in range(len(o_cols)):
+            name = o_cols[i].split(".")
+            if i < len(o_cols)/2:
+                n_name = str(name[0] + "Quant")
+            else:
+                n_name = str(name[0] +"Price")
+            n_cols.append(n_name)
+        
+        tabular.columns = n_cols
+        
+        if tab.index[-1] == tabular.index:
+            print("\n")
+            print("ATTENTION")
+            print("-"*30)
+            print("-"*30)
+            print("The day was already included in the account. Please remove it manually to rewrite")
+            pass
+        else:
+            f_tabular = concat([tab,tabular])
+            f_tabular.to_csv("./accountability.csv")
+            
+            
+def names_members():
+    framy = read_csv("./csv/members_IBEX35.csv")
+    names = framy['Company']
+    return names
+    
 if __name__ == "__main__":
     
     args = arg_parser()
@@ -114,6 +170,10 @@ if __name__ == "__main__":
     total = np.dot(prices, quantities)
     liquid = args.quant - total
     
+    print("\n")
+    print("Using price at date:", prices.to_frame().T.index.values[0])
+    print("-"*30)
+    print("\n")
     print("Number of assets for each equity")
     print("-"*30)
     print("-"*30)
@@ -143,3 +203,7 @@ if __name__ == "__main__":
         print("Total Fees paid", round(fees[2],2))
         print("Absolute fees", round(fees[0],2))
         print("Relative fees",round(fees[1],2))
+        
+    if args.save:
+        saving(quantities, prices)
+        
